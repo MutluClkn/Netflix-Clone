e31db264907ff71791b082086661438b284abab1
Mutlu Çalkan <mutlu.calkan@gmail.com>
2022-12-01 16:23:02 +0300

    UI design created.

---
+++ TMBD Movie App/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMBD Movie App
+//
+//  Created by Mutlu Çalkan on 1.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TMBD Movie App/Views/CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell.swift
+//  TMBD Movie App
+//
+//  Created by Mutlu Çalkan on 1.12.2022.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
