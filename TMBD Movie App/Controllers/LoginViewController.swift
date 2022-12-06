a228cd22d1307069171890884f041931c3c79e55
Mutlu Çalkan <mutlu.calkan@gmail.com>
2022-12-06 10:18:10 +0300

    Login page created.

---
+++ TMBD Movie App/Controllers/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  TMBD Movie App
+//
+//  Created by Mutlu Çalkan on 6.12.2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func forgotPasswordPressed(_ sender: UIButton) {
+    }
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+    }
+    @IBAction func registerButtonPressed(_ sender: UIButton) {
+    }
+}
